a65607f6136c9a57b856fd3f6d1d17659c513cc5
salemaziel <mymainemail0501@gmail.com>
2020-07-27 12:47:01 -0700

    added gatsby firebase theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,6 +78,23 @@ module.exports = {
         icon: `src/images/logo-810x600.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-theme-firebase",
+      options: {
+        credentials: {
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.FIREBASE_DATABASE_URL,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.FIREBASE_APP_ID,
+        },
+        loginPath: "/login",
+        loginRedirectPath: "/dashboard",
+        socialLogins: ["google", "twitter", "facebook", "github"],
+      },
+    },
     `gatsby-plugin-sitemap`,
     {
       resolve: 'gatsby-plugin-robots-txt',
